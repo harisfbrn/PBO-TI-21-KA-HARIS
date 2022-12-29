722ea65977a40cbb9fcb4918c92f4b6b3793bb72
harisfbrn <113957916+harisfbrn@users.noreply.github.com>
2022-12-29 15:23:17 +0700

    Add files via upload

---
+++ Praktikum04/src/Latihan02.java
@@ -0,0 +1,15 @@
+public class Latihan02 {
+    public static void main(String[] args) {
+
+    			int[] num = new int[5];
+    			num[0] = 20;
+    			num[1] = 21;
+    			num[2] = 8;
+    			num[3] = 25;
+    			num[4] = 34;
+
+    			for (int z = 0; z < num.length; z++) {
+    				System.out.println("ages[" + z + "] = " + num[z]);
+    			}
+    		}
+    	}
